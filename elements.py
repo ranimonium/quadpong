b0d531e990eacc96d779c6527a4205fef9ce7fbf
Jadurani Davalos <jadurani.davalos@gmail.com>
2014-03-21 12:27:26 +0800

    moving sprites now

--- elements.py
+++ elements.py
@@ -12,40 +12,59 @@ CBLUE = (100, 149, 237)
 BLACK = (0, 0, 0)
 
 #set up directions = (dx, dy)
+NONE = (0, 0)
 N = (0, -1)
 E = (1, 0)
 W = (-1, 0)
 S = (0, 1)
 NE = (1, -1)
-NW = (-1, 1)
 SE = (1, 1)
+NW = (-1, -1)
 SW = (-1, 1)
+STRTDIR = [N,E,W,S]
+DIAGDIR = [NE,NW,SE,SW]
 
 #returns the supposed direction of the ball when it bounces
-def get_bounceDirection(direction):
-	if direction == NE:
-		return SE
-	elif direction == NW:
-		return SW
-	elif direction == SE:
-		return NE
-	elif direction == SW:
-		return NW
+def get_bounceDirection(direction, to):
+	new_dir = []
+	if to == 'vertical':
+		if direction == NE:
+			new_dir = NW
+		elif direction == NW:
+			new_dir = NE
+		elif direction == SE:
+			new_dir = SW
+		elif direction == SW:
+			new_dir = SE
+	else:
+		if direction == NE:
+			new_dir = SE
+		elif direction == NW:
+			new_dir = SW
+		elif direction == SE:
+			new_dir = NE
+		elif direction == SW:
+			new_dir = NW
+	return new_dir
 
-class Stage:
-	"""docstring for Stage"""
-	MEH = 2
-	TRIANGLE = 3
-	SQUARE = 4
+# class Stage:
+# 	"""docstring for Stage"""
+# 	MEH = 2
+# 	TRIANGLE = 3
+# 	SQUARE = 4
 
-	def __init__(self, edges):
-		self.edges = edges
+# 	def __init__(self, edges):
+# 		self.edges = edges
 
 #sorry I'm not a fan of getters
 		
 #i don't want to use pygame's Sprite 
 class Sprite:
-	"""docstring for Sprite"""
+	"""
+	Mother of Ball and Player
+
+	"""
+
 	def __init__(self, color, pos):
 		self.set_color(color)
 		self.set_pos(pos)
@@ -57,12 +76,22 @@ class Sprite:
 	def set_pos(self, pos):
 		self.x, self.y = pos[0], pos[1]
 
+	def set_direction(self, direction):
+		self.direction = direction
+
+	def update_pos(self, speed):
+		self.set_pos( (self.x + speed*self.direction[0], self.y + speed*self.direction[1]) )
+
 class Ball(Sprite):
-	"""docstring for Ball"""
-	pass
+	
+	def bounce(self, to):
+		print "BOUNCE!"
+		print self.direction
+		self.set_direction(get_bounceDirection(self.direction, to))
+		print self.direction
 		
 class Player(Sprite):
-	"""docstring for Player"""
+	
 	def __init__(self, uid, color, pos):
 		# super(Player, self).__init__(color, pos)
 		Sprite.__init__(self, color, pos)
--- proto4p.py
+++ proto4p.py
@@ -12,14 +12,14 @@ import elements as e
 pygame.init()
 
 # set up the window
-WINDOWWIDTH = 700
-WINDOWHEIGHT = 700
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 400
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('ProtoPong 4')
 
 # set up arena's "dimensions"
-arenaTOP = 75
-arenaLEFT = 75
+arenaTOP = WINDOWHEIGHT/12
+arenaLEFT = WINDOWWIDTH/12
 arenaWIDTH = WINDOWWIDTH - arenaLEFT*2
 arenaHEIGHT = WINDOWHEIGHT - arenaTOP*2
 
@@ -42,7 +42,7 @@ paddleHEIGHT : vertical
 #set up players
 NUM_PLAYERS = 4
 
-paddleWIDTH, paddleHEIGHT = 10, 50
+paddleWIDTH, paddleHEIGHT = 14, 100
 pcolors = [e.CBLUE, e.RED, e.GREEN, e.BLUE]
 pcoords = [
 		[(WINDOWWIDTH)/2 - paddleHEIGHT/2, arenaTOP - 5], #player 0 top
@@ -52,27 +52,50 @@ pcoords = [
 	] #[[x,y],[x,y],[x,y],[x,y]]
 
 players = [e.Player(i, pcolors[i], pcoords[i]) for i in range(NUM_PLAYERS)]
-
+for p in players:
+	p.set_direction(e.NONE)
 
 #set up the ball
-ballWIDTH, ballHEIGHT = 10, 10
-ball = e.Ball( e.AQUA, 
-		[ random.randint(arenaLEFT+150, arenaLEFT+arenaWIDTH-150), #random x position of ball
-		random.randint(arenaTOP+150,arenaTOP+arenaHEIGHT-150)]) #random y position of ball
+ballWIDTH, ballHEIGHT = 14, 14
+ball = e.Ball( e.AQUA, [350, 250])
+		# [ random.randint(arenaLEFT+150, arenaLEFT+arenaWIDTH-150), #random x position of ball
+		# random.randint(arenaTOP+150,arenaTOP+arenaHEIGHT-150)]) #random y position of ball
+ball.set_direction(e.SW)
+# ball.set_direction(random.choice(e.DIAGDIR))
+
+
+def handle_collisions(brect, prects):
+	p_indHit = brect.collidelist(prects)
+	
+	if p_indHit != -1:
+		if p_indHit < 2: #horizontal paddles hit
+			ball.bounce('horizontal')
+		else:
+			ball.bounce('vertical')
+		ball.set_color(players[p_indHit].color)
+
+BSPEED = 1
+
+PSPEED = 2
 
 while True:
 
+	# background
+	windowSurface.fill(e.BLACK)
+
 	# pygame.draw.polygon(windowSurface, (255, 255, 255), [(100, 100), (WINDOWWIDTH-100, 100), (100, WINDOWHEIGHT-100), (WINDOWWIDTH-100, WINDOWHEIGHT-100)], 1)
-	pygame.draw.rect(windowSurface, (255, 255, 255), [arenaLEFT, arenaTOP, arenaHEIGHT, arenaWIDTH], 1)
+	#draw borders
+	pygame.draw.rect(windowSurface, (255, 255, 255), [arenaLEFT, arenaTOP, arenaWIDTH, arenaHEIGHT], 1)
 	
 	# draw the ball
-	pygame.draw.rect(windowSurface, ball.color, [ball.x, ball.y, ballWIDTH, ballHEIGHT])
+	brect = pygame.draw.rect(windowSurface, ball.color, [ball.x, ball.y, ballWIDTH, ballHEIGHT])
 
+	prects = []
 	# draw the paddles
 	for i in range(2):
-		pygame.draw.rect(windowSurface, players[i].color, [players[i].x, players[i].y, paddleHEIGHT, paddleWIDTH])
+		prects.append(pygame.draw.rect(windowSurface, players[i].color, [players[i].x, players[i].y, paddleHEIGHT, paddleWIDTH]))
 	for i in range(2,4):
-		pygame.draw.rect(windowSurface, players[i].color, [players[i].x, players[i].y, paddleWIDTH, paddleHEIGHT]) #proper setting
+		prects.append(pygame.draw.rect(windowSurface, players[i].color, [players[i].x, players[i].y, paddleWIDTH, paddleHEIGHT])) #proper setting
 
 	for event in pygame.event.get():
 		if event.type == QUIT:
@@ -81,8 +104,48 @@ while True:
 		elif event.type == KEYDOWN:
 			if event.key == K_ESCAPE:
 				pygame.event.post(pygame.event.Event(QUIT))
-	
-	#update ball positions
-	#update paddle positions
+			
+			if event.key == K_LEFT: #top
+				players[0].set_direction(e.W)
+			elif event.key == K_RIGHT: #top
+				players[0].set_direction(e.E)
+			elif event.key == K_a: #bottom
+				players[1].set_direction(e.W)
+			elif event.key == K_d: #bottom
+				players[1].set_direction(e.E)
+			elif event.key == K_w: #left
+				players[2].set_direction(e.N)
+			elif event.key == K_s: #left
+				players[2].set_direction(e.S)
+			elif event.key == K_UP: #right
+				players[3].set_direction(e.N)
+			elif event.key == K_DOWN: #right
+				players[3].set_direction(e.S)
 
+
+		elif event.type == KEYUP:
+			if event.key == K_LEFT or event.key == K_RIGHT:  #player 0 top
+				players[0].set_direction(e.NONE)
+			elif event.key == K_a or event.key == K_d: #player 1 bottom
+				players[1].set_direction(e.NONE)
+			elif event.key == K_w or event.key == K_s: #player 2 left
+				players[2].set_direction(e.NONE)
+			elif event.key == K_UP or event.key == K_DOWN: #player 3  right
+				players[3].set_direction(e.NONE)
+	
+
+	#handle collisions
+	handle_collisions(brect, prects)
+	
+	#update ball position
+	ball.update_pos(BSPEED)
+	
+	#update paddle positions
+	for p in players:
+		p.update_pos(PSPEED)
+
+	if ball.x < -10 or ball.x > WINDOWWIDTH+10 or ball.y < -10 or ball.y > WINDOWHEIGHT+10:
+		ball = e.Ball( e.AQUA, [350, 250])
+		ball.set_direction(e.SW)
 	pygame.display.update()
+	time.sleep(0.0009)
